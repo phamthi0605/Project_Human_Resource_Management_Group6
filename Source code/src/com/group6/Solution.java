237357f0d134f7d6d2f15ce4119e5868295dc507
phamthi0605 <116060554+phamthi0605@users.noreply.github.com>
2023-03-17 22:06:32 +0700

    Update menu

--- Source code/src/com/group6/Solution.java
+++ Source code/src/com/group6/Solution.java
@@ -1,15 +1,10 @@
 package com.group6;
 
-import javax.swing.*;
 
 public class Solution {
     public static void main(String[] args) {
         int choose = 0;
-        System.out.println("MANAGEMENT RESOURCE EMPLOYEE");
-        System.out.println("1. Login");
-        System.out.println("2. Management employee");
-        System.out.println("3. Management department");
-        System.out.println("4. Exit");
+        menuProgram();
         System.out.println("Please choose option: ");
         choose = Validation.checkInputInt();
         while (choose != 1 && choose != 4) {
@@ -17,54 +12,80 @@ public class Solution {
             System.out.println("Please choose option again: ");
             choose = Validation.checkInputInt();
         }
+        // Hàm xử lý login ở đây
         if (choose == 1) {
-            // Ham đăng nhập
-            System.out.println("Login successfully!");
-            while (choose < 2 || choose > 6) {
-                System.out.println("Please choose option from 2 to 6: ");
+            System.out.println("Đăng nhập thành công!");
+            while (true) {
+                menuProgram();
+                System.out.println("Vui lòng nhập từ 2 đến 4 để chọn chức năng muốn sử dụng");
                 choose = Validation.checkInputInt();
-            }
-            if (choose == 2) {
-                System.out.println("\tMANAGEMENT EMPLOYEE");
-                System.out.println("\tAdd employee");
-                System.out.println("\tUpdate employee");
-                System.out.println("\tRemove employee");
-                System.out.println("\tFind employee");
-                System.out.println("Please choose option to manage employees:");
-                int option = Validation.checkInputInt();
-                while (option < 1 || choose > 4) {
-                    System.out.println("Please choose option from 1 to 4: ");
-                    option = Validation.checkInputInt();
-                }
-                if (option == 1) {
-                    System.out.println("Add employee");
-                }
-                if (option == 2) {
-                    System.out.println("Update employee");
-                }if (option ==3){
-                    System.out.println("Remove employee");
-                }if (option ==4){
-                    
-                }
+                switch (choose) {
+                    case 2:
+                        System.out.println("\tQUẢN LÝ NHÂN VIÊN");
+                        menuEmployee();
+                        System.out.println("\tChọn chức năng quản lý nhân viên:");
+                        int subOption = Validation.checkInputInt();
+                        while (subOption < 1 || subOption > 5) {
+                            System.out.println("Please choose option again: ");
+                            subOption = Validation.checkInputInt();
+                        }
+                        if (subOption == 1) {
+                            System.out.println("Add employee");
+                            //   break;
+                        }
+                        if (subOption == 2) {
+                            System.out.println("Update employee");
+                        }
+                        if (subOption == 3) {
+                            System.out.println("Remove");
+                        }
+                        if (subOption == 4) {
+                            System.out.println("Find");
+                        }
+                        if (subOption == 5) {
+                            System.out.println("Exit");
+                            break;
+                        }
 
-            }
-            if (choose == 3) {
-                System.out.println("\tMANAGEMENT DEPARTMENT");
-                System.out.println("\tAdd department");
-                System.out.println("\tUpdate department");
-                System.out.println("\tRemove department");
-                System.out.println("\tAdd employee in department");
-                System.out.println("\tRemove employee from department");
-                System.out.println("\tChange department from employee");
-                System.out.println("\tExit");
-
+                        break;
+                    case 3:
+                        System.out.println("Danh sách phòng ban");
+                        break;
+                    case 4:
+                        System.out.println("Exit");
+                        break;
+                }
             }
         }
-        if (choose == 4) {
-            System.out.println("Exit");
-            System.exit(0);
-        }
 
+    }
+
+
+    public static void menuProgram() {
+        System.out.println("MANAGEMENT RESOURCE EMPLOYEE");
+        System.out.println("1. Login");
+        System.out.println("2. Management employee");
+        System.out.println("3. Management department");
+        System.out.println("4. Exit");
+    }
+
+    public static void menuEmployee() {
+        System.out.println("\t1.Add employee");
+        System.out.println("\t2.Update employee");
+        System.out.println("\t3.Remove employee");
+        System.out.println("\t4.Find employee");
+        System.out.println("\t5.Exit");
+    }
+
+    public static void menuDepartment() {
+        System.out.println("\tMANAGEMENT DEPARTMENT");
+        System.out.println("\tAdd department");
+        System.out.println("\tUpdate department");
+        System.out.println("\tRemove department");
+        System.out.println("\tAdd employee in department");
+        System.out.println("\tRemove employee from department");
+        System.out.println("\tChange department from employee");
+        System.out.println("\tExit");
     }
 
 
--- Source code/src/com/group6/Validation.java
+++ Source code/src/com/group6/Validation.java
@@ -16,4 +16,23 @@ public class Validation {
             }
         }
     }
+
+    //check user input yes/ no
+    public static boolean checkInputYN() {
+        //loop until user input correct
+        while (true) {
+            System.out.println("Do you want to continue: ");
+            String result = scanner.nextLine();
+            //return true if user input y/Y
+            if (result.equalsIgnoreCase("Y")) {
+                return true;
+            }
+            //return false if user input n/N
+            if (result.equalsIgnoreCase("N")) {
+                return false;
+            }
+            System.err.println("Please input y/Y or n/N.");
+            System.out.print("Enter again: ");
+        }
+    }
 }
